80e5e7799a11412bd2bd37d1d17dd9adf87fa733
Miller Sofya <miller.sofi@bk.ru>
2024-10-04 22:26:57 +0300

    all pages done

---
+++ script3.js
@@ -0,0 +1,7 @@
+function showContent(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(sectionId).classList.add('active');
+}
